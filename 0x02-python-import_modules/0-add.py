c8a33e578b835e98f48efc796cb37541c7ff04aa
Eboizegbe Osaro <eboizegbeosaro1@gmail.com>
2023-11-30 11:12:56 -0800

    Importing of modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-# This function imports the add_0 function using the import module
-from add_0 import add
 
-# Now initialize variable for the add function
+if __name__ == "__main__":
+    """ This function imports the add_0 function using the import module """
+    from add_0 import add
+    """ Now initialize variable for the add function """
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-''' 
-Have a print statement that would print out
-the value of 'a' and 'b' using the add function
-'''
+    result = add(a, b)
 
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    """ 
+    Have a print statement that would print out
+    the value of 'a' and 'b' using the add function
+    """
+
+    print("{:d} + {:d} = {:d}".format(a, b, result))
