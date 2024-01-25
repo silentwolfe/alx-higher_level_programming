8251f22a84ad57e6d23d2c5e9a2bf1358cd10086
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2024-01-24 22:50:08 -0800

    Printing in 2D with matrices

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
-        print()
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end=" ")
+        print()
