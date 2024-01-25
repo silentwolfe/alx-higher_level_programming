7e74f53d90a9119e8d4e7a471c9ee43dfb88b7b3
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2024-01-25 00:02:36 -0800

    Got rid of the whitespace

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" ")
+    for row in matrix:
+        for i, num in enumerate(row):
+            if i == len(row) - 1:
+                print("{:d}".format(num), end="")
+            else:
+                print("{:d} ".format(num), end="")
         print()
