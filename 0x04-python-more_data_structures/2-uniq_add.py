f41da44991f03d3bb5f3ad86e455e6c514c85067
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2024-01-26 13:17:15 -0800

    The filtering and addition of a list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+        uniq_element = list(set(my_list))
+        sum_total = 0
+        for total in uniq_element:
+            sum_total += total
+        return sum_total
