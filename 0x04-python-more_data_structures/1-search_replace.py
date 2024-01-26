55ad9dbecc68745ffd885fd006aa93ee1c0092a9
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2024-01-26 11:57:48 -0800

    Better implementation, better life

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    found = False
-
-    for index, element in enumerate(new_list):
-        if element == search:
-            new_list[index] = replace
-            found = True
-
-    if not found:
-        print("Element not found")
+    new_list = [replace if element == search else element for element in my_list]
     return new_list
