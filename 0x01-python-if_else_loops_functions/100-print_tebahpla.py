648178db56a4f2ccdcf97b3703e5f3e87d5069d1
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2024-03-13 03:31:55 -0700

    program prints the ascii alphabets in reverse order and alternates them

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for z in range(ord('Z'), ord('A') - 1, -1):
-  print('{0}'.format(chr(z + 32) if z % 2 == 0 else chr(z)), end="")
+    print('{0}'.format(chr(z + 32) if z % 2 == 0 else chr(z)), end="")
