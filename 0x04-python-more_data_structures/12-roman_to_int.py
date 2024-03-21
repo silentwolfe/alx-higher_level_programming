ee9f6e0ffeea583eecad46f4f58bd3cfef1eacc4
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2024-03-21 06:57:07 -0700

    Converting roman to integers

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman):
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    prev_value = 0
+    total = 0
+
+    for num in reversed(roman):
+        value = roman_values[num]
+
+        if value >= prev_value:
+            total += value
+            prev_value = value
+        else:
+            total -= value
+
+    return total
