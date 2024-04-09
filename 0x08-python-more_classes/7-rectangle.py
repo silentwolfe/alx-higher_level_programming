45e3fa7568580fe96a8da796eb60f79c2dd8d276
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2024-04-09 15:01:48 -0700

    commit successful

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+# 7-rectangle.py
+# created by Osaro. e
+
+""" The class Rectangle """
+
+
+class Rectangle:
+    """ In the rectangle class i've added two more methods (Area and perimeter)
+    both used to perform calculations on the Rectangle object """
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rectangle_str = ""
+        for _ in range(self.__height):
+            rectangle_str += str(Rectangle.print_symbol) * self.__width + '\n'
+        return rectangle_str.rstrip()
+
+    def __repr__(self):
+        return f"Rectangle(width={self.__width}, height={self.__height})"
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
