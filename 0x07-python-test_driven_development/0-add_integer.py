4b8d8dc7d52183706de5b99909936d0e00976c3a
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2024-04-09 22:06:32 -0700

    Commit successful

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,15 @@
+#!/usr/bin/python3
+
 def add_integer(a, b=98):
     # Check if a and b are integers or floats
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     # Cast a and b to integers if they are floats
     a = int(a)
     b = int(b)
-    
+
     # Return the sum of a and b
     return a + b
