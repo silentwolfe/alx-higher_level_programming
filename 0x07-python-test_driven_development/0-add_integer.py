11c1d5dcc14b2292d202b069f93afe24d0a7ea21
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2024-04-09 22:11:04 -0700

    Commit successful

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,7 +3,7 @@
 
 
 def add_integer(a, b=98):
-    # Check if a and b are integers or floats
+    """ Check if a and b are integers or floats """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
