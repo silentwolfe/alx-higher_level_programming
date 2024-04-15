9391b9781292dd3300fec6c446a28e0edf95292b
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2024-04-15 21:36:02 +0100

    Python Inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# File - 1-my_list.py
+# Created by Osaro.E
+
+""" Inheriting the methods and properties of the class list. """
+
+
+class MyList(list):
+    
+    """ The function prints out a sorted list.
+    Returns:
+    None
+    """
+
+    def print_sorted(self):
+        print(sorted(self))
