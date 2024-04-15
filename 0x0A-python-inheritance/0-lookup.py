a83d19be07feddd4b86600f8db08871c944b1f7f
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2024-04-15 10:02:48 -0700

    Python inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+# 0-lookup.py
+# created by Osaro. E
+
+""" This function returns a list of all methods and attributes """
+
+def lookup(obj):
+    """ The returning of a list """
+    return list(dir(obj))
