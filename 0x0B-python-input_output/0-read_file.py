fd2b4e7df00a20711edb1e352ef0f4a94c37517c
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2024-04-16 15:36:16 -0700

    File input output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,12 @@
 
 def read_file(filename=""):
     """ Reading from a file """
-    with open(filename, encoding="utf-8") as f:
-        print(f.read())
+    if not filename:
+        print("Provide a file name")
+        return
+    try:
+        with open(filename, encoding="utf-8") as f:
+            for line in f:
+                print(line, end="")
+    except FileNotFoundError:
+        print(f"Couldn't find {filename}")
