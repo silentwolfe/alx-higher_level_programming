db9e1852cc56fb072d6c3367a483635c34fb3f13
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2024-04-18 02:38:54 +0100

    Validating values

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" In progress """
+
+
+class BaseGeometry:
+    """The public instances method of the Basegeometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
