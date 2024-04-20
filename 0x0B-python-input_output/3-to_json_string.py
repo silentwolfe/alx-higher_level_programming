4f25740e0aa6a161b8b97757b44a83ea5b53ba70
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2024-04-20 13:26:27 +0100

    Json decoder

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,14 +1,11 @@
-#!/usr/bin/python3
 import json
+#!/usr/bin/python3
 
-"""_summary_:
-        The function stringifies all object passed into it
-"""
+""" The function stringifies all object passed into it """
 
 
 def to_json_string(my_obj):
-    """_summary_
-
+    """
     Args:
         my_obj (object): The function stringifies
 
